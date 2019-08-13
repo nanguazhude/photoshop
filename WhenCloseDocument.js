3646e032df4492e98fdc9aa90753698d37a316d5
ngzHappy <zhaixueqiang@hotmail.com>
2019-08-13 21:27:38 +0800

    Update WhenCloseDocument.js

--- WhenCloseDocument.js
+++ WhenCloseDocument.js
@@ -1,58 +1,91 @@
 
-function createNextFileName( arg){
+function createNextFileName(arg) {
     var varString = null;
     arg += 1;
-    if(arg<10){
+    if (arg < 10) {
         varString = "000" + arg;
-    }else if(arg<100){
+    } else if (arg < 100) {
         varString = "00" + arg;
-    }else if(arg<1000){
-        varString = "0"+arg;
-    }else{
+    } else if (arg < 1000) {
+        varString = "0" + arg;
+    } else {
         varString = "" + arg;
     }
     return varString + ".jpg";
 }
 
-function saveFileByName(argDoc,argFileName){
-    var jpgFile = new File( argFileName ) ;
-    var jpgSaveOptions = new JPEGSaveOptions() ;
-    jpgSaveOptions.embedColorProfile = true ;
-    jpgSaveOptions.formatOptions = FormatOptions.STANDARDBASELINE ;
-    jpgSaveOptions.matte = MatteType.NONE ;
-    jpgSaveOptions.quality = 8 ;
-    argDoc.saveAs(jpgFile, jpgSaveOptions, true,Extension.LOWERCASE) ;
+function saveFileByName(argDoc, argFileName) {
+    var jpgFile = new File(argFileName);
+    var jpgSaveOptions = new JPEGSaveOptions();
+    jpgSaveOptions.embedColorProfile = true;
+    jpgSaveOptions.formatOptions = FormatOptions.STANDARDBASELINE;
+    jpgSaveOptions.matte = MatteType.NONE;
+    jpgSaveOptions.quality = 8;
+    argDoc.saveAs(jpgFile, jpgSaveOptions, true, Extension.LOWERCASE);
 }
 
-function doApp(){
-    try{
+function doApp() {
+    try {
 
-    var varDoc = app.activeDocument ;
+        var varDoc = app.activeDocument;
 
-    if(varDoc){
-    }else{
+        if (varDoc) {
+        } else {
+            return;
+        }
+
+        //保存文档
+        if (varDoc.saved) {
+        } else {
+            saveFileByName(varDoc, varDoc.fullName);
+        }
+
+        //下一个文件名
+        var varNextFileName = createNextFileName(parseInt(varDoc.name, 10));
+
+        var isOpenedNext = false;
+        {   //寻找在当前文件夹下有无此文件
+            var varNextFileFullName = varDoc.path + "/" + varNextFileName;
+            var varFile = new File(varNextFileFullName);
+            if (varFile && varFile.open("r")) {
+                varFile.close();
+                app.open(varFile);
+                isOpenedNext = true;
+            }
+        }
+
+        if (!isOpenedNext) { 
+            var varThisFolder = new Folder(varDoc.path).parent;
+            var varOpenFiles = null;
+
+            if (varThisFolder) {//寻找在父文件夹下有无此文件
+                varOpenFiles = varThisFolder.getFiles(varNextFileName);
+                if (varOpenFiles && (varOpenFiles.length > 0)) {
+                    app.open(varOpenFiles[0]);
+                    isOpenedNext = true;
+                } else {        //寻找在父文件夹下的所一级子文件下有无此文件
+                    var varNextDirs = varThisFolder.getFiles();
+                    for (var varI = 0; varI < varNextDirs.length; ++varI) {
+                        var varFolderI = varNextDirs[varI];
+                        if (varFolderI instanceof Folder) {
+                            varOpenFiles = varFolderI.getFiles(varNextFileName);
+                            if (varOpenFiles && (varOpenFiles.length > 0)) {
+                                app.open(varOpenFiles[0]);
+                                isOpenedNext = true;
+                                break;
+                            }
+                        }
+                    }/*for*/
+                }/*else*/
+            }/*if*/
+        }
+
+        //关闭文档
+        varDoc.close(SaveOptions.DONOTSAVECHANGES);
+
+    } catch (e) {
         return;
     }
-    
-    //保存文档
-    if(varDoc.saved){
-    }else{
-        saveFileByName(varDoc,varDoc.fullName);
-    }
-    
-    //下一个文件名
-    var varNextFileName = createNextFileName( parseInt(varDoc.name,10) );
-    
-    
-
-    alert( varNextFileName ); 
-    
-    //关闭文档
-    varDoc.close(SaveOptions.DONOTSAVECHANGES);
-
-}catch(e){
-    return;
-}
 
 }
 
@@ -60,4 +93,14 @@ doApp()
 
 
 //https://gitee.com/code_yu/photoshop-javascript
-// Presets\Scripts
+
+//设置快捷键：
+// C:/Program Files/Adobe/Adobe Photoshop CC 2018/Presets/Scripts
+// https://forums.adobe.com/thread/1966460
+
+// https://forums.adobe.com/thread/1246514
+
+//C:\Program Files\Adobe\Adobe Utilities\ExtendScript Toolkit CC\SDK
+
+// https://estk.aenhancers.com/3%20-%20File%20System%20Access/folder-object.html
+// https://estk.aenhancers.com/3%20-%20File%20System%20Access/using-file-and-folder-objects.html
