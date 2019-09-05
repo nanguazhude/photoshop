2a709f97ab12e462d4627e9b715c15c110b8576d
ngzHappy <zhaixueqiang@hotmail.com>
2019-09-05 23:29:45 +0800

    Update PutTextInJPEG.js

--- PutTextInJPEG.js
+++ PutTextInJPEG.js
@@ -8,13 +8,22 @@ function putTheText(argDoc,argString){
     var varTextSize = 36 ;
     var varTextLayer = argDoc.artLayers.add();
     varTextLayer.kind = LayerKind.TEXT;
-    varTextLayer.textItem.font ="方正舒体";
+   // for (i=0; i<  app.fonts.length ; i++) { 
+   //     var varFont = app.fonts[i];
+   //     if(varFont.name  ==  "FZShuTi" ){
+   //      alert(app.fonts[i].postScriptName); 
+   //     }
+   // } 
     varTextLayer.textItem.contents = argString                 ;
+    varTextLayer.textItem.font = "FZSTK--GBK1-0" ; /* "方正舒体" */;
     varTextLayer.textItem.size = varTextSize                   ;
-    varTextLayer.textItem.kind = TextType.PARAGRAPHTEXT        ;
+    //varTextLayer.textItem.kind = TextType.PARAGRAPHTEXT        ;
     varTextLayer.textItem.justification = Justification.LEFT   ;
-    var varTextXPos = (argDoc.width - varTextSize ) - (varTextLayer.bounds[2] - varTextLayer.bounds[0]);
-    varTextLayer.textItem.position = new Array(varTextXPos , varTextSize + 8 );
+    var varSpace = 16 ;
+    var varTextXPos = (argDoc.width - varSpace ) - (varTextLayer.bounds[2] - varTextLayer.bounds[0]);
+    varTextLayer.textItem.position = new Array(
+        varTextXPos , 
+        varSpace + varTextLayer.bounds[3] - varTextLayer.bounds[1] );
 
 }
 
@@ -77,7 +86,59 @@ function doTheApp(){
 doTheApp()
 
 
+/**** 
+新細明體：PMingLiU
 
+細明體：MingLiU
+
+標楷體：DFKai-SB
+
+黑体：SimHei
+
+新宋体：NSimSun
+
+仿宋：FangSong
+
+楷体：KaiTi
+
+仿宋_GB2312：FangSong_GB2312
+
+楷体_GB2312：KaiTi_GB2312
+
+微軟正黑體：Microsoft JhengHei
+
+微软雅黑体：Microsoft YaHei
+
+装Office会生出来的一些：
+
+隶书：LiSu
+
+幼圆：YouYuan
+
+华文细黑：STXihei
+
+华文楷体：STKaiti
+
+华文宋体：STSong
+
+华文中宋：STZhongsong
+
+华文仿宋：STFangsong
+
+方正舒体：FZShuTi
+
+方正姚体：FZYaoti
+
+华文彩云：STCaiyun
+
+华文琥珀：STHupo
+
+华文隶书：STLiti
+
+华文行楷：STXingkai
+
+华文新魏：STXinwei
+*/
 
 
 
